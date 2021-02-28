fc4202837f577d4b6971d3098a5d7a37b8b16085
ta987ka123 <60844480+ta987ka123@users.noreply.github.com>
2021-02-28 23:09:44 +0900

    20210228

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ const onClickAdd = () => {
   //テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ作成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
